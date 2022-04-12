4961d376e9cb2e65d384d61dc3d0635f11631afe
Bryan <brayanby2002@gmail.com>
2022-04-12 13:40:42 -0500

    Proyecto crud de producto -> reg/listado/buscar

--- src/main/java/app/FrmManteProd.java
+++ src/main/java/app/FrmManteProd.java
@@ -142,10 +142,33 @@ public class FrmManteProd extends JFrame {
 		cboProveedores.setBounds(300, 104, 120, 22);
 		contentPane.add(cboProveedores);
 		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				buscar();
+			}
+		});
+		btnBuscar.setBounds(324, 63, 89, 23);
+		contentPane.add(btnBuscar);
+		
 		llenaCombo();
 		llenaComboProveedor();
 	}
 
+	void buscar() {
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("mysql");
+		EntityManager em = fabrica.createEntityManager();		
+		
+		Producto p = em.find(Producto.class, txtCódigo.getText());
+		
+		txtDescripcion.setText(p.getDescripcion());
+		cboCategorias.setSelectedIndex(p.getIdcategoria());
+		txtPrecio.setText(Double.toString(p.getPrecio()));
+		txtStock.setText(Integer.toString(p.getStock()));
+		cboProveedores.setSelectedItem(p.getProveedor().getNombre());
+		em.close();	
+	}
+	
 	void llenaCombo() {
 		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("mysql");
 		EntityManager em = fabrica.createEntityManager();
